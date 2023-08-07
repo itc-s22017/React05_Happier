38dd44fe05e8cef094d9ae7dd3e48ad07bc107c3
s22030 <s22030@std.it-college.ac.jp>
2023-08-07 04:20:17 +0000

    ボタン作成

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <button class="favorite styled" type="button">Add to favorites</button>
+    </div>
+  )
+}
+
+export default Home
