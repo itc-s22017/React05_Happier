b343c4bd270ba7c22e5a86fc7ca85ffdc29157a8
itc-22017 <s22017@std.it-college.ac.jp>
2023-08-14 02:31:56 +0000

    オヤジギャグAPIリクエスト成功

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios"
+import "./Home.css"
+
 
 function Home() {
+  const [joke,setJoke] = useState("")
+
+  const headers = {
+    'Content-Type': 'application/json',
+  }
+
+  const handleJoke = async() => {
+    const response = await axios.get("https://icanhazdadjoke.com/slack")
+    setJoke(response.data.attachments[0].text)
+  }
   return (
-    <div>ここにオヤジギャグページ</div>
+    <div>
+      <p>{joke ? joke : "TEXT"}</p>
+      <button onClick={handleJoke}>push</button>
+    </div>
   )
 }
 
