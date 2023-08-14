ca413276bf0e67eb36c79b5b5b5ca4074ff3e5ec
s22030 <s22030@std.it-college.ac.jp>
2023-08-14 01:05:37 +0000

    gitpullするためのコミット

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        <button class="favorite styled" type="button">Add to favorites</button>
+         <Button variant="primary">Primary</Button>
     </div>
   )
 }
